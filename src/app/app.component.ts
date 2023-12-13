f56e352beb82adccc2e79dca09a830895b6bb5dd
Josip Anic <josipanic1985@gmail.com>
2023-12-13 14:47:11 +0100

    Format code and integrate LoginComponent into the app
    The LoginComponent class has been added, along with its associated HTML template and Cypress test case. The AppComponent has been adjusted to include LoginComponent. Various parts of the code have been reformatted for improved readability, including the HTML templates of LoginComponent and AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {LoginComponent} from "./auth/login/login.component";
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,8 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
