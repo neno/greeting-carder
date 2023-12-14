12b82ea2bc988f960c0532ab1305b237013b7dad
Josip Anic <josipanic1985@gmail.com>
2023-12-14 15:45:59 +0100

    Add SignupComponent and integrate it in the application
    Created SignupComponent which includes an HTML form for user registration, respective TypeScript file defining the form controls and validation rules, and also added corresponding test file. Integrated SignupComponent in AppComponent to include in the application user interface.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import {SignupComponent} from "./auth/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  signupForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    name: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8), Validators.maxLength(32)]),
+    passwordConfirm: new FormControl("")
+  })
+
+  get password() {
+    return this.signupForm.get('password')
+  }
+
+  get email() {
+    return this.signupForm.get('email')
+  }
+
+  get name() {
+    return this.signupForm.get('name')
+  }
+
+  get passwordConfirm() {
+    return this.signupForm.get('passwordConfirm')
+  }
+
+  log() {
+    console.log('singup');
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
